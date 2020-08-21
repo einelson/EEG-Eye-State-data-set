10b797f1341995debb7f85abe57a390ce5eff994
Ethan Nelson <36721383+einelson8i@users.noreply.github.com>
2020-08-21 11:10:50 -0600

    Model image render

--- eeg.py
+++ eeg.py
@@ -9,7 +9,7 @@ from scipy import stats
 from keras.utils import normalize
 from keras.utils import np_utils
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout
+from keras.layers import Dense, Activation, Dropout, LSTM, Embedding
 from keras.optimizers import Adam
 
 from sklearn.model_selection import train_test_split
@@ -17,6 +17,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.preprocessing import StandardScaler
 
 import tensorflow.keras
+import keras
 
 
 home='C:/Users/einel/Documents/projects/EEG-Eye-State-data-set'
@@ -59,22 +60,48 @@ run_logdir, run_id = get_run_logdir(root_logdir)
 tensorboard_cb = tensorflow.keras.callbacks.TensorBoard(run_logdir)
 
 # create  sequential model
-model = Sequential()
+'''model = Sequential()
 model.add(Dense(256, input_dim=14))
 model.add(Dense(256, activation='relu'))
 model.add(Dense(128))
 model.add(Dense(256))
 model.add(Dense(2, activation='sigmoid'))
-print(model.summary())
+print(model.summary())'''
 
 
 # create functional model
+inputs=keras.Input(shape=(14,))
+
+dense = Dense(200, activation='relu')
+x=dense(inputs)
+
+x=Dense(200, activation='relu')(x)
+
+x=Dense(100, activation='relu')(x)
+
+outputs = Dense(2, activation='sigmoid')(x)
+
+model=keras.Model(inputs=inputs, outputs=outputs, name="eeg_model")
+
+keras.utils.plot_model(model, "./data/eeg_model.png")
 
 
-# Try RNN network
+# Try RNN network- needs work
+'''model = Sequential()
+# Add an Embedding layer expecting input vocab of size 1000, and
+# output embedding dimension of size 64.
+model.add(Dense(100, input_dim=14))
+
+# Add a LSTM layer with 128 internal units.
+model.add(LSTM(128))
+
+# Add a Dense layer with 10 units.
+model.add(Dense(10))
+
+model.add(Dense(2))'''
 
 
-
+model.summary()
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
 model.fit(x=xTrain, y=yTrain,epochs=100,batch_size=32, validation_data=(xTest,yTest))
 
