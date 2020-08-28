ba31fbae93dc2c3a7c74bdd5f39bbf3d335dd940
Ethan Nelson <36721383+einelson8i@users.noreply.github.com>
2020-08-28 10:07:58 -0600

    edited model
    96.5% acc

--- eeg.py
+++ eeg.py
@@ -9,7 +9,7 @@ from scipy import stats
 from keras.utils import normalize
 from keras.utils import np_utils
 from keras.models import Sequential
-from keras.layers import Dense, Activation, Dropout, LSTM, Embedding, add
+from keras.layers import Dense, Activation, Dropout, LSTM, Embedding, concatenate, add
 from keras.optimizers import Adam
 
 from sklearn.model_selection import train_test_split
@@ -50,73 +50,41 @@ data = scaler.transform(data)
 data_targets=np_utils.to_categorical(data_targets, num_classes=2)
 
 # split the data
-xTrain, xTest, yTrain, yTest = train_test_split(data, data_targets, test_size = 0.2)
-# print(xTrain, yTrain)
+xTrain, xTest, yTrain, yTest = train_test_split(data, data_targets, test_size = 0.1)
 
 
 # prepare save files
 run_logdir, run_id = get_run_logdir(root_logdir)
-# print(run_logdir)
 tensorboard_cb = tensorflow.keras.callbacks.TensorBoard(run_logdir)
 
-# create  sequential model
-'''model = Sequential()
-model.add(Dense(256, input_dim=14))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(128))
-model.add(Dense(256))
-model.add(Dense(2, activation='sigmoid'))
-print(model.summary())'''
-
-
-# create functional model
+# # create functional model
+# Block 1
 inputs=keras.Input(shape=(14,))
-x=Dense(200, activation='relu')(inputs)
-x=Dense(200, activation='relu')(x)
-block_1_output = Dense(100, activation='sigmoid')(x)
+x=Dense(256)(inputs)
+x=Dense(256, activation='relu')(x)
+x=Dense(128)(x)
+block_1_output = Dense(256)(x)
 
-x=Dense(100, Activation='relu')(block_1_output)
-x=Dense(100, activation='relu')(x)
-x=Dense(200, activation='relu')(x)
+# # Block 2
+# x=Dense(150, activation='relu')(block_1_output)
+# # x=Dense(100, activation='relu')(x)
+# x=Dense(150, activation='relu')(x)
+# block_2_output = concatenate([x, block_1_output])
 
-block_2_output = add([x, block_1_output])
-
-outputs = keras.layers.Dense(2)(block_2_output)
+outputs =Dense(2, activation='sigmoid')(block_1_output)
 
 
 model=keras.Model(inputs=inputs, outputs=outputs, name="eeg_model")
-
 keras.utils.plot_model(model, "./data/eeg_model.png", show_shapes=True)
-
-
-# Try RNN network- needs work
-'''model = Sequential()
-# Add an Embedding layer expecting input vocab of size 1000, and
-# output embedding dimension of size 64.
-model.add(Dense(100, input_dim=14))
-
-# Add a LSTM layer with 128 internal units.
-model.add(LSTM(128))
-
-# Add a Dense layer with 10 units.
-model.add(Dense(10))
-
-model.add(Dense(2))'''
-
 
 model.summary()
 model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-model.fit(x=xTrain, y=yTrain,epochs=100,batch_size=32, validation_data=(xTest,yTest))
+model.fit(x=xTrain, y=yTrain,epochs=500,batch_size=256, validation_data=(xTest,yTest))
 
 score, acc = model.evaluate(x=xTest, y=yTest)
-# print(model.predict(xTest))
+network = model.predict(xTest)
 
 print('Accuracy: ',100*(acc))
-
-
-# back to home directory
-# print(home)
-# save the model
+# back to home directory, save the model
 new_dir=home+'/data/'+str(100*acc)+'_'+run_id+'.h5'
 model.save(new_dir)
-
